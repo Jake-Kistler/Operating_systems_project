c36afcf61a8462709185aac78ffe9f6a6409d9d4
Jake <jakistler01@gmail.com>
2025-03-10 15:48:05 -0500

    Starting to merge things over[JK: 3/10/2025: 3:47 PM]

--- CS3113_Project2.cpp
+++ CS3113_Project2.cpp
@@ -121,7 +121,14 @@ void executeCPU(int startAddress, int *mainMemory);
 
 void checkIOWaitingQueue(std::queue<int> &readyQueue, int *mainMemory);
 
-void allocateMemory(int process_id, int size, MemoryBlock *memory_head);
+// Memory block functions
+void allocateMemory(MemoryBlock *&memoryHead, int process_id, int size);
+
+void printMemoryBlocks(MemoryBlock* memoryHead);
+
+void deallocateMemory(MemoryBlock *&memory_head, int process_id);
+
+void coalesceMemory(MemoryBlock *&memory_head);
 
 int main(int argc, char **argv)
 {
@@ -510,26 +517,50 @@ void checkIOWaitingQueue(std::queue<int> &readyQueue, int *mainMemory)
     }
 }
 
-void allocateMemory(int process_id, int size, MemoryBlock *memory_head)
+/*
+* MEMORY BLOCK FUNCTIONS
+*/
+MemoryBlock *iterateMemory(MemoryBlock *current)
+{
+    // If the current current block exists return the next block in the list
+    if(current != nullptr)
+        return current->next;
+    
+    // if current is null, we are are at the end of the list so return null
+    return nullptr;
+}
+
+// Function to allocate memory for a process
+void allocateMemory(MemoryBlock *&memory_head, int process_id, int size)
 {
     MemoryBlock *current = memory_head;
+
+    std::cout << "DEBUG: Trying to allocate Process " << process_id << " with size " << size << "\n";
+
 
     // need to search for a free memory block IE when process_id is -1 and see if the size of this block will work 
     while (current) // will stop when current == nullptr 
     {
-        if(current->process_id == -1 && current->block_size == size)
+        std::cout << "DEBUG: Checking block at start " << current->start_address << " with size " << current->block_size << " (Process ID: " << current->process_id << ")\n\n";
+
+        if(current->process_id == -1 && current->block_size >= size)
         {
-            int former_size = current->block_size; // store the orginal size of the block
+            std::cout << "DEBUG: Found free block! Assigning Process " << process_id << "\n\n";
+
+            int former_size = current->block_size; // store the original size of the block
             current->process_id = process_id; // assign the process ID to this block marking it for use
             current->block_size = size; // update the size of this block to the asked for size
 
             // we need to create a new block should there be unallocated memory
             if(former_size > size)
             {
-                MemoryBlock  *new_block = new MemoryBlock(-1, current->start_address + size, 
-                    former_size - size); // starts with the -1 (free) ID, it starts at the current address's start + the size, the size of this new block is the left over memory
+                MemoryBlock* new_block = new MemoryBlock(-1, current->start_address + size, former_size - size);
 
                 new_block->next = current->next; //linking it back to the linked list 
+
+                std::cout << "DEBUG: Split block. New free block at " 
+                          << new_block->start_address << " with size " << new_block->block_size << "\n\n";
+
                 current->next = new_block; // insert it AFTER the newly allocated block so it [allocated block] ... [new block]
             }
 
@@ -543,3 +574,72 @@ void allocateMemory(int process_id, int size, MemoryBlock *memory_head)
     std::cout << "Process " << process_id << " waiting in NewJobQueue due to insufficient memory.\n";
     
 } // END allocateMemory
+
+// Function to print the memory allocation
+void printMemoryBlocks(MemoryBlock* memoryHead) 
+{
+    MemoryBlock* current = memoryHead;
+    std::cout << "\nCurrent Memory Blocks:\n";
+
+    for (MemoryBlock* current = memoryHead; current != nullptr; current = iterateMemory(current)) {
+        std::cout << "[ " << (current->process_id == -1 ? "Free" : "P" + std::to_string(current->process_id))
+                  << " | Start=" << current->start_address
+                  << " | Size=" << current->block_size << " ] -> ";
+    }
+
+    std::cout << "NULL\n";
+}
+
+void deallocateMemory(MemoryBlock *&memory_head, int process_id)
+{
+    MemoryBlock *current = memory_head;
+
+    std::cout <<"DEBUG: Trying to deallocate process " << process_id << "\n";
+
+    while(current)
+    {
+        if(current->process_id == process_id) // Found the process block number
+        {
+            std::cout << "DEBUG: Found process " << process_id << " at start address " << current->start_address << ", freeing memory. \n";
+            current->process_id = -1; // Mark as free
+
+            std::cout << "Process " << process_id << " terminated and released memory from " << current->start_address << " to " << (current->start_address + current->block_size) << "\n";
+
+
+            //call coalesceMemory to merge adjacent free blocks
+            coalesceMemory(memory_head);
+            return;
+            
+        }
+
+        current = current->next; // move to the next node in the list
+    }
+
+    std::cout << "WARING: process " << process_id << " not found in memory\n";
+}
+
+void coalesceMemory(MemoryBlock *&memory_head)
+{
+    MemoryBlock *current = memory_head;
+
+    std::cout << "DEBUG: checking for memory coalescing...\n";
+
+    while(current && current->next)
+    {
+        if(current->process_id == -1 && current->next->process_id == -1)
+        {
+            std::cout << "DEBUG: Merging free blocks at " << current->start_address << " and " << current->next->start_address << "\n";
+
+            current->block_size += current->next->block_size; // expand the current block to include the next free block
+            MemoryBlock *temp = current->next;
+            current->next = current->next->next; // cut out the now merged block from the list
+            delete temp; // free the memory
+
+            std::cout << "Memory coalesced at address " << current->start_address << " with new size " << current->block_size << "\n";
+        }
+        else
+        {
+            current = current->next;
+        }
+    }
+}
